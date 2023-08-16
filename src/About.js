586f6f8df50cd5d9eaf8d82cc9ab10557ac23357
Matt Walter <matt.walter.tx@outlook.com>
2023-08-16 08:25:32 -0500

    Setup routes w/ home, about and badurl

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+
+  return (
+    <div>
+      <p>About content will go here</p>
+    </div>
+  )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./Home.js";
+import About from "./About.js";
+import BadURL from "./BadURL.js"
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={(<Home />)} />
+        <Route path="about" element={(<About />)} />
+        <Route path="/*" element={(<BadURL />)} />
+      </Routes>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
