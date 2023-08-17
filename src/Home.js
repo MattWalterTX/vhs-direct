24d975aac0a4e5dea4bdca80a7cac2234b45cd2c
Matt Walter <matt.walter.tx@outlook.com>
2023-08-17 09:31:07 -0500

    Update chunk sizes

--- src/Home.js
+++ src/Home.js
@@ -5,11 +5,11 @@ const Home = () => {
 
   return (
     <div>
-      <div className="test">
         <div className="header">
           <h4>VHS Direct</h4>
           <button>SIGN IN</button>
         </div>
+      <div className="chunk">
         <div className="content-box">
           <h3>SKIP THE WAIT</h3>
           <p>GET VHS CASSETTES STREAMED DIRECTLY TO YOUR PREFRONTAL CORTEX</p>
@@ -19,9 +19,9 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="test">
+      <div className="chunk">
         <div className="img-container">
-          <div className="test">
+          <div>
             <h3>THE BEST ESCAPISM MONEY CAN BUY.</h3>
             <p>GIVE YOUR BRAIN A BREAK FROM WORK. WE'LL TAKE GOOD CARE OF IT WHILE YOU'RE AWAY</p>
           </div>
@@ -30,18 +30,18 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="test">
+      <div className="chunk">
          <div className="img-container">
           <div>
             <p>IMG goes here</p>
           </div>
-          <div className="test">
+          <div>
             <h3>EMBRACE THE SPOOK.</h3>
             <p>UNLEASH THE HALLOWEEN SPIRIT UPON YOUR UNSUSPECTING PEERS EVERY DAY OF THE YEAR.</p>
           </div>
         </div>
       </div>
-      <div className="test">
+      <div className="chunk">
         <div className="content-box">
             <h3>NOT READY TO SEAL YOUR FATE?</h3>
             <p>BROWSE LIBRARY FOR FREE</p>
